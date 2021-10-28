9419bb5eac3aa8253aa01f6cba027d49dd0b0649
liudmila-budanitskaya-code <Liudmila28march1991@gmail.com>
2021-10-28 10:29:51 +0300

    added settings menu button

--- app/src/main/java/by/budanitskaya/l/quilixtest/ui/currencyinfo/CurrencyInfoFragment.kt
+++ app/src/main/java/by/budanitskaya/l/quilixtest/ui/currencyinfo/CurrencyInfoFragment.kt
@@ -1,10 +1,8 @@
 package by.budanitskaya.l.quilixtest.ui.currencyinfo
 
 import android.os.Bundle
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.ViewModelProvider
 import by.budanitskaya.l.quilixtest.R
@@ -31,6 +29,8 @@ class CurrencyInfoFragment : Fragment() {
             R.layout.fragment_currency_info, container, false
         )
 
+        setHasOptionsMenu(true)
+
         return binding.root
     }
 
@@ -43,5 +43,19 @@ class CurrencyInfoFragment : Fragment() {
         viewModel.message.observe(viewLifecycleOwner, {
             binding.textViewData.text = it
         })
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.menu_settings, menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.settings -> {
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
     }
 }
